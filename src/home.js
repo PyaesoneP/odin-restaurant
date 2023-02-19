a6cbc813e2f23d4944c297faa75853df80366a9d
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-02-19 22:07:26 +0630

    Do the home page hero section

--- src/home.js
+++ src/home.js
@@ -1,9 +1,31 @@
 function renderHome() {
   function render() {
-    const container = document.getElementById("content");
-    const heading = document.createElement("p");
-    heading.innerText = "Home";
-    container.appendChild(heading);
+    const contentContainer = document.getElementById("content-container");
+    const heroContainer = document.createElement("div");
+    heroContainer.id = "hero-container";
+    const popularMealsContainer = document.createElement("div");
+
+    const heroImgDiv = document.createElement("div");
+    const heroTextDiv = document.createElement("div");
+
+    const heroImage = document.createElement("img");
+    heroImage.setAttribute("src", "../src/heroimg.jpg");
+    heroImage.id = "hero-image";
+    heroImgDiv.appendChild(heroImage);
+
+    const heroHeader = document.createElement("h1");
+    heroHeader.id = "hero-header";
+    heroHeader.innerText = "Welcome to RESTAURANT";
+    const heroText = document.createElement("p");
+    heroText.id = "hero-text";
+    heroText.innerText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    heroTextDiv.appendChild(heroHeader);
+    heroTextDiv.appendChild(heroText);
+
+    heroContainer.appendChild(heroImgDiv);
+    heroContainer.appendChild(heroTextDiv);
+    contentContainer.appendChild(heroContainer);
   }
   return { render };
 }
--- src/index.js
+++ src/index.js
@@ -18,8 +18,11 @@ import { renderMenu } from "./menu";
     nav.id = "nav";
     logo.innerText = "RESTAURANT";
     navLink1.innerText = "Home";
+    navLink1.id = "home";
     navLink2.innerText = "Menu";
+    navLink2.id = "menu";
     navLink3.innerText = "Contact";
+    navLink3.id = "contact";
 
     logoDiv.appendChild(logo);
     navLinks.appendChild(navLink1);
@@ -33,22 +36,29 @@ import { renderMenu } from "./menu";
   })();
 
   (function renderPage() {
-    const homeButton = document.getElementById("home-button");
-    const menuButton = document.getElementById("menu-button");
-    const contactButton = document.getElementById("contact-button");
+    const container = document.getElementById("content");
+    const contentContainer = document.createElement("div");
+    contentContainer.id = "content-container";
+    container.appendChild(contentContainer);
 
-    homeButton.addEventListener("click", () => {
-      const page = renderHome();
+    const home = document.getElementById("home");
+    const menu = document.getElementById("menu");
+    const contact = document.getElementById("contact");
+    let page = renderHome();
+    page.render();
+
+    home.addEventListener("click", () => {
+      page = renderHome();
       page.render();
     });
 
-    menuButton.addEventListener("click", () => {
-      const page = renderMenu();
+    menu.addEventListener("click", () => {
+      page = renderMenu();
       page.render();
     });
 
-    contactButton.addEventListener("click", () => {
-      const page = renderContact();
+    contact.addEventListener("click", () => {
+      page = renderContact();
       page.render();
     });
   })();
